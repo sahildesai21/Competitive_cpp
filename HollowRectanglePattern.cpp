e15ccd72fbe7469b511d2f842a540ecf6c0a7106
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2024-05-10 23:21:03 +0530

    Add files via upload

---
+++ HollowRectanglePattern.cpp
@@ -0,0 +1,36 @@
+//Code for Hallow Rectangle pattern
+/* Ex-
+******
+*    *
+*    *
+*    *
+******
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    int rows=0,cols=0;
+    cout<<"enter the Rows and Coloumns: "<<endl;
+    cin>>rows>>cols;
+    for (int i=1;i<=rows;i++)
+    {
+        for (int j=1;j<=cols;j++)
+        {
+            if(i==1 || i==rows ){
+                cout<<"*";
+            }
+            else if (j==1 || j==cols)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+};
