39aabed94d1ab9908797ae52b143e6f734922a5d
sahildesai21 <desaisahil.2104@gmail.com>
2024-05-12 15:19:01 +0530

    half pyramid Usinf Numbers

---
+++ HalfPyramidUsingNumbers.cpp
@@ -0,0 +1,28 @@
+/*
+Code for half pyramid Using numbers
+ex-5
+1
+22
+333
+4444
+55555
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    for (int i=1;i<=num;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+};
